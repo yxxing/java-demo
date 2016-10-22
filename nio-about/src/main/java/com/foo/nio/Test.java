d8ad95c302eebd30027dcb740bd4fb3db84766c5
xingyuexi <xingyuexi@tuniu.com>
2016-10-22 09:27:36 +0800

    test environment

---
+++ nio-about/src/main/java/com/foo/nio/Test.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright (C) 2006-2016 All rights reserved
+ */
+package com.foo.nio;
+
+/**
+ * Just for test
+ * Date: 2016-10-22
+ *
+ * @author xingyuexi
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("hello world!");
+    }
+}
